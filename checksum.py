4b4d23badb26f9c0106eeb7985da5b2f6251f9b7
Villa Devoto LSA <rios.jesus.francisco.41@gmail.com>
2020-05-20 20:36:31 -0300

    Hamming terminado

--- checksum.py
+++ checksum.py
@@ -1,2 +1,3 @@
+''' https://es.wikipedia.org/wiki/Suma_de_verificaci%C3%B3n '''
 def checksum(Cadena):
      
--- hamming.py
+++ hamming.py
@@ -1,4 +1,5 @@
 ''' VERIFICA SOLO CADENAS DE 7 BITS '''
+''' https://es.wikipedia.org/wiki/C%C3%B3digo_Hamming '''
 def hamming(cadena):
     print(cadena)
     ## Primero Verificar es binarios y si tiene  7 bits 
@@ -6,13 +7,37 @@ def hamming(cadena):
 
     if binario:
         ## Obtener los 4 bits de paridad
-		copyCadena = cadena.copy()
-		copyCadena.pop(2)
-		copyCadena.pop(5)
-		print(copyCadena)
-		p1 = getParidad(copyCadena)
+        copyCadena = cadena.copy()
+        copyCadena.pop(2)
+        copyCadena.pop(4)
+        print(copyCadena)
+        p1 = getParidad(copyCadena)
         
-		## Agregar bit de paridad en la cadena
+        copyCadena = cadena.copy()
+        copyCadena.pop(4)
+        copyCadena.pop(1)
+        print(copyCadena)
+        p2 = getParidad(copyCadena)
+
+        copyCadena = cadena.copy()
+        copyCadena.pop(6)
+        copyCadena.pop(5)
+        copyCadena.pop(4)
+        copyCadena.pop(0)
+        print(copyCadena)
+        p3 = getParidad(copyCadena)
+
+        copyCadena = cadena.copy()
+        copyCadena.pop(3)
+        copyCadena.pop(2)
+        copyCadena.pop(1)
+        copyCadena.pop(0)
+        print(copyCadena)
+        p4 = getParidad(copyCadena)
+
+        ## Agregar bits de paridad en la cadena
+        cadenaHamming = juntarCadena(cadena, p1, p2, p3, p4)
+        print(cadenaHamming)
  
 def isBinario(cadena):
     binario = False
@@ -36,9 +61,12 @@ def getParidad(cadena):
             if(cadena[i] == 1):
                 unos = unos + 1
     if(unos % 2 == 0):
-        return "PAR"
-    return "IMPAR"
+        return 0
+    return 1
 
+def juntarCadena(cadena, p1, p2, p3, p4):
+    cadenaFinal = [p1, p2, cadena[0], p3, cadena[1], cadena[2], cadena[3], p4, cadena[4], cadena[5], cadena[6]]
+    return cadenaFinal
 
 if __name__== "__main__":
-    hamming([1,0,0,0,1,0,1])
+    hamming([0,1,1,0,1,0,1])
