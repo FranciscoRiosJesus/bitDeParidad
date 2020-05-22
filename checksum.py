e3cf8112c241fe20188671da5e8600386e8cb543
Villa Devoto LSA <rios.jesus.francisco.41@gmail.com>
2020-05-21 23:46:51 -0300

    Funciona CheckSum.py

--- checksum.py
+++ checksum.py
@@ -1,3 +1,36 @@
 ''' https://es.wikipedia.org/wiki/Suma_de_verificaci%C3%B3n '''
-def checksum(Cadena):
-     
+
+def checksum(cadena1, cadena2, cadena3):
+    # suma de arrays
+    cadenaSumada = sumar(cadena1, cadena2, cadena3)
+    print(bin(cadenaSumada))
+    # complemento de cadenaSumada
+    cadenaComplemento = complemento(cadenaSumada)
+    print(bin(cadenaComplemento))
+
+    status = chequeo(cadena1, cadena2, cadena3, cadenaComplemento)
+
+    if status:
+        print("CheckSum: " + bin(cadena1) + " - " + bin(cadena2) + " - " + bin(cadena3) + " - " + bin(cadenaComplemento) + " = " + bin(cadena1+cadena2+cadena3+cadenaComplemento))
+
+
+def sumar(cadena1, cadena2, cadena3):
+    return (cadena1 + cadena2 + cadena3)
+
+
+def complemento(cadenaSumada):
+    #No Funciona
+    return -((cadenaSumada) - int('1111111111111111',2))
+
+
+def chequeo(cadena1, cadena2, cadena3, cadenaComplemento):
+    
+    return True
+
+if __name__== "__main__":
+    A = int('0110011001100110',2)
+    B = int('0101010101010101',2)
+    C = int('0000111100001111',2)
+    D = int('0011010100110101',2)
+    print(bin(A+B+C+D))
+    checksum(A, B, C)
